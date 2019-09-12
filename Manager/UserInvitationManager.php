07456b7cd550b2b4a8e71a599ffaf301d8e53c91
Javi H. Gil <javihgil@gmail.com>
2019-09-12 10:34:06 +0200

    Invitation acceptance also make the confirmation (email has been confirmed)

--- Manager/UserInvitationManager.php
+++ Manager/UserInvitationManager.php
@@ -2,10 +2,11 @@
 
 namespace Softspring\UserBundle\Manager;
 
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
-use Softspring\UserBundle\Manager\UserInvitationManagerInterface;
-use Softspring\UserBundle\Manager\UserManagerInterface;
+use Softspring\UserBundle\Model\ConfirmableInterface;
+use Softspring\UserBundle\Model\NameSurnameInterface;
 use Softspring\UserBundle\Model\UserInterface;
 use Softspring\UserBundle\Model\UserInvitationInterface;
 
@@ -23,8 +24,8 @@ class UserInvitationManager implements UserInvitationManagerInterface
 
     /**
      * UserInvitationManager constructor.
-     *
      * @param EntityManagerInterface $em
+     * @param UserManagerInterface $userManager
      */
     public function __construct(EntityManagerInterface $em, UserManagerInterface $userManager)
     {
@@ -46,7 +47,10 @@ class UserInvitationManager implements UserInvitationManagerInterface
      */
     public function getRepository(): EntityRepository
     {
-        return $this->em->getRepository(UserInvitationInterface::class);
+        /** @var EntityRepository $repo */
+        $repo = $this->em->getRepository(UserInvitationInterface::class);
+
+        return $repo;
     }
 
     /**
@@ -67,17 +71,23 @@ class UserInvitationManager implements UserInvitationManagerInterface
 
         $user->setUsername($invitation->getUsername());
         $user->setEmail($invitation->getEmail());
-        $user->setName($invitation->getName());
-        $user->setSurname($invitation->getSurname());
+
+        if ($user instanceof NameSurnameInterface) {
+            $user->setName($invitation->getName());
+            $user->setSurname($invitation->getSurname());
+        }
+
         $user->setRoles($invitation->getRoles());
+
+        if ($user instanceof ConfirmableInterface) {
+            $user->setConfirmedAt(new DateTime('now'));
+        }
 
         return $user;
     }
 
     /**
      * @param UserInvitationInterface $userInvitation
-     *
-     * @throws \Exception
      */
     public function save(UserInvitationInterface $userInvitation): void
     {
@@ -90,7 +100,10 @@ class UserInvitationManager implements UserInvitationManagerInterface
      */
     public function findInvitationBy(array $criteria): ?UserInvitationInterface
     {
-        return $this->getRepository()->findOneBy($criteria);
+        /** @var UserInvitationInterface|null $invitation */
+        $invitation = $this->getRepository()->findOneBy($criteria);
+
+        return $invitation;
     }
 
     /**
