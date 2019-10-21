4807bbc088f881ceca0805db941b966c415a6040
Javi H. Gil <javihgil@gmail.com>
2019-10-21 12:41:06 +0200

    Use SfsCoreBundle doctrine target entity resolver abstract compiler pass

--- DependencyInjection/Compiler/ResolveDoctrineTargetEntityPass.php
+++ DependencyInjection/Compiler/ResolveDoctrineTargetEntityPass.php
@@ -2,54 +2,29 @@
 
 namespace Softspring\UserBundle\DependencyInjection\Compiler;
 
+use Softspring\CoreBundle\DependencyInjection\Compiler\AbstractResolveDoctrineTargetEntityPass;
 use Softspring\UserBundle\Model\UserAccessInterface;
 use Softspring\UserBundle\Model\UserInterface;
 use Softspring\UserBundle\Model\UserInvitationInterface;
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Exception\LogicException;
 
-class ResolveDoctrineTargetEntityPass implements CompilerPassInterface
+class ResolveDoctrineTargetEntityPass extends AbstractResolveDoctrineTargetEntityPass
 {
+    /**
+     * @inheritDoc
+     */
+    protected function getEntityManagerName(ContainerBuilder $container): string
+    {
+        return $container->getParameter('sfs_user.entity_manager_name');
+    }
+
     /**
      * @param ContainerBuilder $container
      */
     public function process(ContainerBuilder $container)
     {
-        // configure user entity
-        $userClass = $container->getParameter('sfs_user.user.class');
-        if (!class_implements($userClass, UserInterface::class)) {
-            throw new LogicException(sprintf('%s class must implements %s interface', $userClass, UserInterface::class));
-        }
-        $this->setTargetEntity($container, UserInterface::class, $userClass);
-
-        // configure invitation entity
-        if ($userInvitationClass = $container->getParameter('sfs_user.invite.class')) {
-            if (!class_implements($userInvitationClass, UserInvitationInterface::class)) {
-                throw new LogicException(sprintf('%s class must implements %s interface', $userInvitationClass, UserInvitationInterface::class));
-            }
-
-            $this->setTargetEntity($container, UserInvitationInterface::class, $userInvitationClass);
-        }
-
-        // configure user access history entity
-        if ($historyClass = $container->getParameter('sfs_user.history.class')) {
-            if (!class_implements($historyClass, UserAccessInterface::class)) {
-                throw new LogicException(sprintf('%s class must implements %s interface', $historyClass, UserAccessInterface::class));
-            }
-
-            $this->setTargetEntity($container, UserAccessInterface::class, $historyClass);
-        }
-    }
-
-    private function setTargetEntity(ContainerBuilder $container, string $interface, string $class)
-    {
-        $resolveTargetEntityListener = $container->findDefinition('doctrine.orm.listeners.resolve_target_entity');
-
-        if (!$resolveTargetEntityListener->hasTag('doctrine.event_subscriber')) {
-            $resolveTargetEntityListener->addTag('doctrine.event_subscriber');
-        }
-
-        $resolveTargetEntityListener->addMethodCall('addResolveTargetEntity', [$interface, $class, [$container->getParameter('sfs_user.entity_manager_name')]]);
+        $this->setTargetEntityFromParameter('sfs_user.user.class', UserInterface::class, $container, true);
+        $this->setTargetEntityFromParameter('sfs_user.invite.class', UserInvitationInterface::class, $container, false);
+        $this->setTargetEntityFromParameter('sfs_user.history.class', UserAccessInterface::class, $container, false);
     }
 }
