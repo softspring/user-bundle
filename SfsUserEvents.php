9da85d531d5109f78d3a2b0235e1eb59b298a210
Javi H. Gil <javihgil@gmail.com>
2020-09-28 09:50:35 +0200

    Update events definition

--- SfsUserEvents.php
+++ SfsUserEvents.php
@@ -13,13 +13,13 @@ class SfsUserEvents
     /** @Event("Softspring\UserBundle\Event\GetResponseUserEvent") */
     const REGISTER_INITIALIZE = 'sfs_user.register.initialize' ;
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const REGISTER_FORM_VALID = 'sfs_user.register.form_valid' ;
 
     /** @Event("Softspring\UserBundle\Event\GetResponseUserEvent") */
     const REGISTER_SUCCESS = 'sfs_user.register.success' ;
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const REGISTER_FORM_INVALID = 'sfs_user.register.form_invalid' ;
 
     /** @Event("Softspring\CoreBundle\Event\ViewEvent") */
@@ -37,7 +37,7 @@ class SfsUserEvents
     /** @Event("Softspring\UserBundle\Event\GetResponseUserEvent") */
     const INVITATION_ACCEPTED = 'sfs_user.invitation.accepted' ;
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const INVITATION_FORM_INVALID = 'sfs_user.invitation.form_valid' ;
 
     /** @Event("Softspring\UserBundle\Event\GetResponseUserEvent") */
@@ -52,55 +52,55 @@ class SfsUserEvents
     /** @Event("Softspring\UserBundle\Event\UserEvent") */
     const LOGIN_IMPLICIT = 'sfs_user.login.implicit' ;
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const LOGIN_ATTEMPT = 'sfs_user.login.attempt' ;
 
     /** @Event("Softspring\UserBundle\Event\GetResponseUserEvent") */
     const PREFERENCES_UPDATED = 'sfs_user.preferences.updated';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const PREFERENCES_FORM_VALID = 'sfs_user.preferences.form_valid';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const PREFERENCES_FORM_INVALID = 'sfs_user.preferences.form_invalid';
 
     /** @Event("Softspring\UserBundle\Event\GetResponseUserEvent") */
     const CHANGE_EMAIL_UPDATED = 'sfs_user.change_email.updated';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const CHANGE_EMAIL_FORM_VALID = 'sfs_user.change_email.form_valid';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const CHANGE_EMAIL_FORM_INVALID = 'sfs_user.change_email.form_invalid';
 
     /** @Event("Softspring\UserBundle\Event\GetResponseUserEvent") */
     const CHANGE_PASSWORD_UPDATED = 'sfs_user.change_password.updated';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const CHANGE_PASSWORD_FORM_VALID = 'sfs_user.change_password.form_valid';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const CHANGE_PASSWORD_FORM_INVALID = 'sfs_user.change_password.form_invalid';
 
     /** @Event("Softspring\UserBundle\Event\GetResponseUserEvent") */
     const CHANGE_USERNAME_UPDATED = 'sfs_user.change_username.updated';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const CHANGE_USERNAME_FORM_VALID = 'sfs_user.change_username.form_valid';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const CHANGE_USERNAME_FORM_INVALID = 'sfs_user.change_username.form_invalid';
 
     /** @Event("Softspring\UserBundle\Event\GetResponseEvent") */
     const RESET_REQUEST_INITIALIZE = 'sfs_user.reset_request.initialize';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const RESET_REQUEST_FORM_VALID = 'sfs_user.reset_request.form_valid';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const RESET_REQUEST_DO_REQUEST = 'sfs_user.reset_request.do_request';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const RESET_REQUEST_FORM_INVALID = 'sfs_user.reset_request.form_invalid';
 
     /** @Event("Softspring\CoreBundle\Event\ViewEvent") */
@@ -112,13 +112,13 @@ class SfsUserEvents
     /** @Event("Softspring\UserBundle\Event\GetResponseEvent") */
     const RESET_INITIALIZE = 'sfs_user.reset.initialize';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const RESET_FORM_VALID = 'sfs_user.reset.form_valid';
 
     /** @Event("Softspring\UserBundle\Event\GetResponseUserEvent") */
     const RESET_SUCCESS = 'sfs_user.reset.success';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const RESET_FORM_INVALID = 'sfs_user.reset.form_invalid';
 
     /** @Event("Softspring\CoreBundle\Event\ViewEvent") */
@@ -142,13 +142,13 @@ class SfsUserEvents
     /** @Event("Softspring\UserBundle\Event\GetResponseUserEvent") */
     const ADMIN_USERS_DELETE_INITIALIZE = 'sfs_user.admin.users.delete_initialize';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const ADMIN_USERS_DELETE_FORM_VALID = 'sfs_user.admin.users.delete_form_valid';
 
     /** @Event("Softspring\UserBundle\Event\GetResponseUserEvent") */
     const ADMIN_USERS_DELETE_SUCCESS = 'sfs_user.admin.users.delete_success';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const ADMIN_USERS_DELETE_FORM_INVALID = 'sfs_user.admin.users.delete_form_invalid';
 
     /** @Event("Softspring\CoreBundle\Event\ViewEvent") */
@@ -157,13 +157,13 @@ class SfsUserEvents
     /** @Event("Softspring\UserBundle\Event\GetResponseUserEvent") */
     const ADMIN_USERS_UPDATE_INITIALIZE = 'sfs_user.admin.users.update_initialize';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const ADMIN_USERS_UPDATE_FORM_VALID = 'sfs_user.admin.users.update_form_valid';
 
     /** @Event("Softspring\UserBundle\Event\GetResponseUserEvent") */
     const ADMIN_USERS_UPDATE_SUCCESS = 'sfs_user.admin.users.update_success';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const ADMIN_USERS_UPDATE_FORM_INVALID = 'sfs_user.admin.users.update_form_invalid';
 
     /** @Event("Softspring\CoreBundle\Event\ViewEvent") */
@@ -196,13 +196,13 @@ class SfsUserEvents
     /** @Event("Softspring\UserBundle\Event\GetResponseUserEvent") */
     const ADMIN_ADMINISTRATORS_DELETE_INITIALIZE = 'sfs_user.admin.administrators.delete_initialize';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const ADMIN_ADMINISTRATORS_DELETE_FORM_VALID = 'sfs_user.admin.administrators.delete_form_valid';
 
     /** @Event("Softspring\UserBundle\Event\GetResponseUserEvent") */
     const ADMIN_ADMINISTRATORS_DELETE_SUCCESS = 'sfs_user.admin.administrators.delete_success';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const ADMIN_ADMINISTRATORS_DELETE_FORM_INVALID = 'sfs_user.admin.administrators.delete_form_invalid';
 
     /** @Event("Softspring\CoreBundle\Event\ViewEvent") */
@@ -211,13 +211,13 @@ class SfsUserEvents
     /** @Event("Softspring\UserBundle\Event\GetResponseUserEvent") */
     const ADMIN_ADMINISTRATORS_UPDATE_INITIALIZE = 'sfs_user.admin.administrators.update_initialize';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const ADMIN_ADMINISTRATORS_UPDATE_FORM_VALID = 'sfs_user.admin.administrators.update_form_valid';
 
     /** @Event("Softspring\UserBundle\Event\GetResponseUserEvent") */
     const ADMIN_ADMINISTRATORS_UPDATE_SUCCESS = 'sfs_user.admin.administrators.update_success';
 
-    /** @Event("Softspring\UserBundle\Event\GetResponseFormEvent") */
+    /** @Event("Softspring\CoreBundle\Event\GetResponseFormEvent") */
     const ADMIN_ADMINISTRATORS_UPDATE_FORM_INVALID = 'sfs_user.admin.administrators.update_form_invalid';
 
     /** @Event("Softspring\CoreBundle\Event\ViewEvent") */
