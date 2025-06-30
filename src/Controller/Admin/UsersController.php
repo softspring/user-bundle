92ea3a92bd93d81a03f9326cc3707df60362f21c
AritzPalaciosOrtiz <46780004+AritzPalaciosOrtiz@users.noreply.github.com>
2025-06-30 10:35:19 +0200

    Confirm user (#24)
    * Confirm user
    
    * Confirm user
    
    * Confirm user
    
    * Confirm user
    
    * Confirm user
    
    * Confirm user
    
    * Confirm user
    
    * Confirm user
    
    * Confirm user
    
    * Confirm user
    
    * Confirm user
    
    * Confirm user
    
    * Confirm user
    
    ---------
    
    Co-authored-by: Javi H. Gil <javihgil@users.noreply.github.com>

--- src/Controller/Admin/UsersController.php
+++ src/Controller/Admin/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace Softspring\UserBundle\Controller\Admin;
 
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Softspring\Component\Events\DispatchGetResponseTrait;
@@ -9,9 +10,12 @@ use Softspring\UserBundle\Event\GetResponseUserEvent;
 use Softspring\UserBundle\Mailer\UserMailerInterface;
 use Softspring\UserBundle\Manager\UserManagerInterface;
 use Softspring\UserBundle\Model\ConfirmableInterface;
+use Softspring\UserBundle\Model\EnablableInterface;
 use Softspring\UserBundle\Model\RolesAdminInterface;
+use Softspring\UserBundle\Model\User;
 use Softspring\UserBundle\Model\UserInterface;
 use Softspring\UserBundle\SfsUserEvents;
+use Softspring\UserBundle\Util\TokenGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -30,12 +34,15 @@ class UsersController extends AbstractController
 
     protected EventDispatcherInterface $eventDispatcher;
 
-    public function __construct(UserManagerInterface $userManager, EntityManagerInterface $em, ?UserMailerInterface $userMailer, EventDispatcherInterface $eventDispatcher)
+    protected TokenGeneratorInterface $tokenGenerator;
+
+    public function __construct(UserManagerInterface $userManager, EntityManagerInterface $em, ?UserMailerInterface $userMailer, EventDispatcherInterface $eventDispatcher, TokenGeneratorInterface $tokenGenerator)
     {
         $this->userManager = $userManager;
         $this->em = $em;
         $this->userMailer = $userMailer;
         $this->eventDispatcher = $eventDispatcher;
+        $this->tokenGenerator = $tokenGenerator;
     }
 
     public function promoteAdmin(string $user, Request $request): Response
@@ -82,6 +89,76 @@ class UsersController extends AbstractController
         return $this->render('@SfsUser/admin/users/widget-pending-confirm-count.html.twig', [
             'count' => $this->userManager->getRepository()->count(['confirmedAt' => null]),
         ]);
+    }
+
+    public function userConfirm(string $user): Response
+    {
+        /** @var User $user */
+        $user = $this->userManager->findUserBy(['id' => $user]);
+
+        $this->denyAccessUnlessGranted('PERMISSION_SFS_USER_ADMIN_USERS_CONFIRM', $user);
+
+        if (!$user instanceof UserInterface || !$user instanceof ConfirmableInterface) {
+            throw new Exception(sprintf('User %s class must implement %s to confirm', get_class($user), ConfirmableInterface::class));
+        }
+
+        $user->setConfirmationToken(null);
+        $user->setConfirmedAt(new DateTime());
+        $this->userManager->saveEntity($user);
+
+        return $this->redirectToRoute('sfs_user_admin_users_details', ['user' => $user->getId()]);
+    }
+
+    public function userUnconfirm(string $user): Response
+    {
+        /** @var User $user */
+        $user = $this->userManager->findUserBy(['id' => $user]);
+
+        $this->denyAccessUnlessGranted('PERMISSION_SFS_USER_ADMIN_USERS_UNCONFIRM', $user);
+
+        if (!$user instanceof UserInterface || !$user instanceof ConfirmableInterface) {
+            throw new Exception(sprintf('User %s class must implement %s to confirm', get_class($user), ConfirmableInterface::class));
+        }
+
+        $user->setConfirmationToken($this->tokenGenerator->generateToken());
+        $user->setConfirmedAt(null);
+        $this->userManager->saveEntity($user);
+
+        return $this->redirectToRoute('sfs_user_admin_users_details', ['user' => $user->getId()]);
+    }
+
+    public function userEnable(string $user): Response
+    {
+        /** @var User $user */
+        $user = $this->userManager->findUserBy(['id' => $user]);
+
+        $this->denyAccessUnlessGranted('PERMISSION_SFS_USER_ADMIN_USERS_ENABLE', $user);
+
+        if (!$user instanceof UserInterface || !$user instanceof EnablableInterface) {
+            throw new Exception(sprintf('User %s class must implement %s to enable', get_class($user), EnablableInterface::class));
+        }
+
+        $user->setEnabled(true);
+        $this->userManager->saveEntity($user);
+
+        return $this->redirectToRoute('sfs_user_admin_users_details', ['user' => $user->getId()]);
+    }
+
+    public function userDisable(string $user): Response
+    {
+        /** @var User $user */
+        $user = $this->userManager->findUserBy(['id' => $user]);
+
+        $this->denyAccessUnlessGranted('PERMISSION_SFS_USER_ADMIN_USERS_DISABLE', $user);
+
+        if (!$user instanceof UserInterface || !$user instanceof EnablableInterface) {
+            throw new Exception(sprintf('User %s class must implement %s to enable', get_class($user), EnablableInterface::class));
+        }
+
+        $user->setEnabled(false);
+        $this->userManager->saveEntity($user);
+
+        return $this->redirectToRoute('sfs_user_admin_users_details', ['user' => $user->getId()]);
     }
 
     public function resendConfirmationEmail(string $user, Request $request): Response
