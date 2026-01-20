69440f0cb17cdea4bbeff22b5375ab28b7038c2f
aritz <aritz@softspring.es>
2026-01-20 16:14:16 +0100

    Mailer exceptions

--- src/Controller/Settings/ConfirmationController.php
+++ src/Controller/Settings/ConfirmationController.php
@@ -14,7 +14,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 
 class ConfirmationController extends AbstractController
 {
