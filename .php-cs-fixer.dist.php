65dd81056edb3672aaa9b20d1e46e599e37c7fd1
Javi H. Gil <javihgil@softspring.es>
2026-03-27 13:57:05 +0100

    Fix ci

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,17 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+    ->notPath([
+        'config/bundles.php',
+        'config/reference.php',
+    ])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+    ])
+    ->setFinder($finder)
+;
